9b79d5a4eb16b5c9be940fd8848d2b95a1802dd1
smxky <tallamichael007@gmail.com>
2025-05-01 18:49:23 +0100

    added the auth module routes middleware to the app file

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,27 @@ import express, { Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 
+import { departmentRouter } from "./routes/department.route";
+import { employeeRouter } from "./routes/employee.route";
+import { guestRouter } from "./routes/guest.route";
+import { permissionRouter } from "./routes/permission.route";
+import { permissionOverideRouter } from "./routes/permissionOveride.route";
+import { positionRouter } from "./routes/position.route";
+import { roleRouter } from "./routes/role.route";
+import { rolePermissionRouter } from "./routes/rolePermission.route";
+import { userRouter } from "./routes/user.route";
+
 export const app = express();
 
 //Use this as reference for api comments
 /**
  * @openapi
- * /users:
+ * /:
  *   get:
- *     summary: Get all users
+ *     summary: Test to if the Server is Running
  *     responses:
  *       200:
- *         description: A list of users.
+ *         description: Testing if the server is online.
  *         content:
  *           application/json:
  *             schema:
@@ -24,6 +34,16 @@ export const app = express();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from the Atlantic Pearl Hotel and Resort API...");
 });
+
+app.use("/api/v1/departments", departmentRouter);
+app.use("/api/v1/employees", employeeRouter);
+app.use("/api/v1/guests", guestRouter);
+app.use("/api/v1/permissions", permissionRouter);
+app.use("/api/v1/permissionOverides", permissionOverideRouter);
+app.use("/api/v1/positions", positionRouter);
+app.use("/api/v1/roles", roleRouter);
+app.use("/api/v1/rolePermissions", rolePermissionRouter);
+app.use("/api/v1/users", userRouter);
 
 const swaggerSpec = swaggerJSDoc({
   definition: {
